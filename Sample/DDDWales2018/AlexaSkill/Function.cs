6ab697ff3306065690044c3ad78a0a2fb50729ad
Steven Pears <steven.pears@live.co.uk>
2018-03-18 12:08:02 +0000

    push response and function

--- Sample/DDDWales2018/AlexaSkill/Function.cs
+++ Sample/DDDWales2018/AlexaSkill/Function.cs
@@ -1,27 +1,72 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace AlexaSkill
 {
+
     public class Function
     {
-        
-        /// <summary>
-        /// A simple function that takes a string and does a ToUpper
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            return input?.ToUpper();
+
+            switch (input.Request)
+            {
+                case LaunchRequest _:
+                    return ResponseBuilder.Ask(Responses.Welcome, null);
+
+                case IntentRequest intent:
+                    if (intent.Intent.Name == BuiltInIntent.Cancel|| intent.Intent.Name == BuiltInIntent.Stop)
+                    {
+                        return ResponseBuilder.Tell("Okay");
+                    }
+
+                    if (HandleIntent(input, intent.Intent, out SkillResponse response))
+                    {
+                        return response;
+                    }
+                    break;
+            }
+
+            return ResponseBuilder.Ask("Sorry, didn't understand a word. Please try that again", null);
+        }
+
+
+
+
+
+
+
+
+
+
+        private bool HandleIntent(SkillRequest request, Intent intent, out SkillResponse skillResponse)
+        {
+            skillResponse = null;
+            switch (intent.Name)
+            {
+                case "NewGame":
+                    skillResponse = ResponseBuilder.Tell("Old News");
+                    return true;
+                case "StartGame":
+                    if (intent.Slots.Any(s => string.IsNullOrWhiteSpace(s.Value.Value)))
+                    {
+                        skillResponse = ResponseBuilder.DialogDelegate(request.Session);
+                        return true;
+                    }
+
+                    skillResponse = ResponseBuilder.Tell("Game stuff coming soon!");
+                    return true;
+
+            }
+            return false;
         }
     }
 }
---
+++ Sample/DDDWales2018/AlexaSkill/Responses.cs
@@ -0,0 +1,7 @@
+namespace AlexaSkill
+{
+    public class Responses
+    {
+        public const string Welcome = "Welcome to rock, paper, scissors, lizard, spock. Do you want to start a game or check your challenges?";
+    }
+}
