179817f795151743935c1cb00ff0a609b4bc7d96
Steven Pears <steven.pears@live.co.uk>
2018-03-21 22:11:35 +0000

    refactor out notification turn welcome into ssml

--- Sample/DDDWales2018/AlexaSkill/Function.cs
+++ Sample/DDDWales2018/AlexaSkill/Function.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Alexa.NET;
-using Alexa.NET.Notifications;
 using Alexa.NET.Request;
 using Alexa.NET.Request.Type;
 using Alexa.NET.Response;
 using Alexa.NET.SkillMessaging;
 using Amazon.Lambda.Core;
-using Amazon.Runtime;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -25,7 +22,7 @@ namespace AlexaSkill
 
         public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
         {
-            Console.WriteLine("Received Request Type: "+input.Request.Type);
+            Console.WriteLine("Received Request Type: " + input.Request.Type);
             switch (input.Request)
             {
                 case LaunchRequest _:
@@ -38,60 +35,22 @@ namespace AlexaSkill
                     }
                     break;
                 case MessageReceivedRequest message:
-                    await SendNotification(input,message);
+                    await GameNotification.Send(input, message);
                     return ResponseBuilder.Empty();
             }
 
             return ResponseBuilder.Ask("Sorry, didn't understand a word. Please try that again", null);
         }
 
-        private Task SendNotification(SkillRequest request, MessageReceivedRequest message)
-        {
-
-            var token = request.Context.System.ApiAccessToken;
-            var challengerName = message.Message["from"];
-            var notification = new NotificationClient(NotificationClient.EuropeEndpoint,token);
-
-            var display = new DisplayInfo{Content = new List<DisplayContent>
-                {
-                    DisplayContent(challengerName,"en-US"),
-                    DisplayContent(challengerName,"en-GB")
-                }
-            };
-            var spoken = new SpokenInfo
-            {
-                Content = new List<SpokenText>
-                {
-                    new SpokenText("en-US","You've a new game of rock paper scissors lizard spock"),
-                    new SpokenText("en-GB","You've a new game of rock paper scissors lizard spock")
-                }
-            };
-            var reference = Guid.NewGuid().ToString("N");
-            var expiry = DateTime.Now.AddSeconds(30);
-
-            return notification.Create(display, spoken, reference, expiry);
-        }
-
-        private DisplayContent DisplayContent(string challengerName, string locale)
-        {
-            return new DisplayContent
-            {
-                Title = $"A new challenge has been issued",
-                Body = $"{challengerName} has challenged you to a game at DDD Wales",
-                Toast = new ContentItem("A new challenge has been issued"),
-                Locale = locale
-            };
-        }
-
-
         private Task<SkillResponse> HandleIntent(SkillRequest request, Intent intent)
         {
             switch (intent.Name)
             {
-                case BuiltInIntent.Stop:case BuiltInIntent.Cancel:
-                    return Task.FromResult(ResponseBuilder.Tell(Responses.Close));
+                case BuiltInIntent.Stop:
+                case BuiltInIntent.Cancel:
+                    return CloseGame.Handle();
                 case IntentNames.StartGame:
-                    return StartGame.Handle(request,intent);
+                    return StartGame.Handle(request, intent);
                 case IntentNames.CheckChallenges:
                     return CheckChallenge.Handle(request, intent);
                 case IntentNames.Moves:
@@ -102,4 +61,5 @@ namespace AlexaSkill
         }
 
     }
+}
 }
--- Sample/DDDWales2018/AlexaSkill/Responses.cs
+++ Sample/DDDWales2018/AlexaSkill/Responses.cs
@@ -1,14 +1,27 @@
+using System.Collections.Generic;
 using System.Reflection.Metadata;
 using Alexa.NET.Response;
+using Alexa.NET.Response.Ssml;
 
 namespace AlexaSkill
 {
     public static class Responses
     {
+        public static Speech Welcome = new Speech{
+                Elements = new List<ISsml>
+                {
+                    new Paragraph{
+                        Elements = new List<IParagraphSsml>{
+                            new PlainText("Welcome to"),
+                            new Prosody{Rate = ProsodyRate.Fast,Elements = new List<ISsml>{new Sentence("rock paper scissors lizard spock") }},
+                            new Sentence("Do you want to start a game or check your challenges?")
+                    }
+                }
+        }};
+
         public const string Draw = "It's a draw, maybe you'll get the upper hand next time";
         public const string Win = "You won! Well done. Hope you have another game soon";
         public const string Loss = "You didn't win this time, maybe next time";
-        public const string Welcome = "Welcome to rock paper scissors lizard spock. Do you want to start a game or check your challenges?";
         public const string GameCreated = "The challenge has been sent, check your challenges to find out the result";
         public const string NoChallenges = "No challenges so far, please try again soon";
         public const string ChallengeLength = "You have {0} challenges available right now.";
