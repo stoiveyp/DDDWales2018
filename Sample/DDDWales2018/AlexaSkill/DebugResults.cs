5c5fd39152db533ad62af8069bf6002442baffc4
Steven Pears <steven.pears@live.co.uk>
2018-03-23 16:06:19 +0000

    Initial alexa (#5)
    Initial Alexa skill and structure

--- Sample/DDDWales2018/AlexaSkill/DebugResults.cs
+++ Sample/DDDWales2018/AlexaSkill/DebugResults.cs
@@ -14,6 +14,7 @@ namespace AlexaSkill
             {
                 return Task.FromResult(ResponseBuilder.DialogDelegate(request.Session, intent));
             }
+            //TODO: Produce Results
         }
     }
 }
